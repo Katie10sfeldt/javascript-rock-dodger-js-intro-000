9e788e902068c6f6b7eff05816a96b7fa469f158
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-11 17:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -153,8 +153,6 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-
-   var leftNumbers = positionToInteger(DODGER.style.left);
    var rightNumbers = positionToInteger(DODGER.style.right);
 
    if (rightNumbers < 0) {
