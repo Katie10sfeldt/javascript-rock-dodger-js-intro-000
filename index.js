5bf52a18b71d20d9a2f398445c5b165ea86a1b75
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-11 18:01:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,24 +42,13 @@ function checkCollision(rock) {
     if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) {
       return true;
     }
-    if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) {
+    else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) {
       return true;
     }
-    if (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge) {
+    else if (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge) {
       return true
-    }
-
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge.
-               */) {
-      return true
+    } else {
+      return false;
     }
   }
   return false;
